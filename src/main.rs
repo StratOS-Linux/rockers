558fdfb4e2d9f0bf8c964d1e6156a53625dba82b
Magitian <magitian@duck.com>
2024-02-24 13:33:03 +0530

    Added a function to detect installed package managers

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,20 @@ fn install_pkg(pkgmgr: &str, inst_cmd: &str, pkg: &str) {
 	println!("{pkgmgr} {inst_cmd} {pkg}");
 }
 
-// fn installed_sources(pkg: &str) {
-// 	let sources: Vec<&str> = vec!();
-// }
+fn installed_sources() -> Vec<&'static str> {
+	let mut sources: Vec<&str> = vec!();
+	if pkgmgr_found("/usr/bin/pacman") { sources.push("pacman"); }
+	if pkgmgr_found("/usr/bin/yay")    { sources.push("yay"); }
+	if pkgmgr_found("/usr/bin/apt")    { sources.push("apt"); }
+	if pkgmgr_found("/usr/bin/flatpak")    { sources.push("apt"); }
+	return sources;
+}
 
 fn main() {
 	let args = CliArgs::parse();
 	let mut install_cmd = "";
 	
+	println!("Package managers detected: {:?}", installed_sources());
 	// println!("Command: {:?} Pkg: {:?}", args.rockcmd, args.pkgname);
 
 	if pkgmgr_found("/usr/bin/pacman") {
@@ -34,7 +40,6 @@ fn main() {
 
 	match args.rockcmd.as_str() {
 		"install"|"i" => install_pkg("pacman", &install_cmd, &args.pkgname),
-		// "install" => println!("-S"),
 		&_ => println!("Invalid"),
 	};
 }
