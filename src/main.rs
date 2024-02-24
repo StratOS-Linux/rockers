f3deb4d00af62dadd0c3de74a61f60aefe7afa8c
Magitian <magitian@duck.com>
2024-02-24 18:35:15 +0530

    Made search work, pkginfo yet to be completed, run_shell_cmd() doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+#[allow(dead_code)]
 use clap::Parser;
 use std::path::Path;
-use std::process::{Command, Stdio};
+use std::process::{Command, Stdio, exit};
+
+// const BLACK: &str = "\x1B[30m";
+const BLUE: &str = "\x1B[34m";
+// const YELLOW: &str = "\x1B[33m";
+// const GREEN: &str = "\x1B[32m";
+// const RED: &str = "\x1B[31m";
+const RESET: &str = "\x1B[0m";
 
 #[derive(Parser)]
 struct CliArgs {
@@ -15,9 +23,22 @@ fn pkgmgr_found(p: &str) -> bool {
 	return false;
 }
 
-fn install_pkg(pkgmgr: &str, inst_cmd: &str, pkg: &str) {
-	println!("{pkgmgr} {inst_cmd} {pkg}");
+fn run_shell_cmd(command: &str) -> String {
+	let output = Command::new("bash")
+		.arg("-c")
+		.arg(command)
+		.output()
+		.unwrap_or_else(|e| {
+			eprintln!("Unable to run cmd: {}", e);
+			exit(1);
+		});
+	return String::from_utf8_lossy(&output.stdout).trim().to_string();
 }
+
+// fn pkg_install_src(pkg: &str) -> Vec<&'static str> {
+// 	let mut sources: Vec<&str> = vec!();
+	
+// }
 
 fn installed_sources() -> Vec<&'static str> {
 	let mut sources: Vec<&str> = vec!();
@@ -26,6 +47,24 @@ fn installed_sources() -> Vec<&'static str> {
 	if pkgmgr_found("/usr/bin/apt")    { sources.push("apt"); }
 	if pkgmgr_found("/usr/bin/flatpak")    { sources.push("apt"); }
 	return sources;
+}
+
+fn install_pkg(pkgmgr: &str, inst_cmd: &str, pkg: &str) {
+	println!("{pkgmgr} {inst_cmd} {pkg}"); // TODO
+}
+
+fn info_pkg(pkgmgr: &str, info_cmd: &str, pkg: &str) {
+	if pkgmgr=="pacman" {
+		let output = Command::new(pkgmgr)
+			.args([info_cmd, pkg, " | grep -i Validated | awk '{print $4}"])
+			.stdout(Stdio::piped())
+			.output()
+			.unwrap();
+		let result = String::from_utf8(output.stdout).unwrap();
+		for line in result.lines() {
+			println!("{}{RESET}", line);
+		}
+	}
 }
 
 fn search_pkg(pkgmgr: &str, search_cmd: &str, pkg: &str) {
@@ -38,8 +77,8 @@ fn search_pkg(pkgmgr: &str, search_cmd: &str, pkg: &str) {
 
 	for line in result.lines() {
 		if pkgmgr=="pacman" && line.contains("[installed]") {
-			print!("\x1B[34m{}\x1B[0m\n", line);
-		} else {
+			println!("{BLUE}{}{RESET}", line);
+ 		} else {
 			println!("{}", line);
 		}
 	}
@@ -48,18 +87,23 @@ fn search_pkg(pkgmgr: &str, search_cmd: &str, pkg: &str) {
 fn main() {
 	let args = CliArgs::parse();
 	let mut install_cmd = "";
+	let mut search_cmd = "";
+	let mut info_cmd = "";
 	
 	println!("Package managers detected: {:?}", installed_sources());
 	// println!("Command: {:?} Pkg: {:?}", args.rockcmd, args.pkgname);
 
 	if pkgmgr_found("/usr/bin/pacman") {
 		install_cmd = "-S";
+		search_cmd = "-Ss";
+		info_cmd = "-Qi"
 	}
 
 	match args.rockcmd.as_str() {
-		"install"|"i" => install_pkg("pacman", &install_cmd, &args.pkgname),
+		"install" | "i" => install_pkg("pacman", &install_cmd, &args.pkgname),
+		"search"  | "s" => search_pkg("pacman", &search_cmd, &args.pkgname),
+		"info"    | "I" => info_pkg("pacman", &info_cmd, &args.pkgname),
 		&_ => println!("Invalid"),
 	};
 
-	search_pkg("pacman", "-Ss", "hyprland");
 }
