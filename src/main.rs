5d6be0ae686fbaf77c4375bf8389b43274632cf2
Magitian <magitian@duck.com>
2024-02-24 12:10:43 +0530

    Got basic Pacman parsing for -S

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::path::Path;
 
 #[derive(Parser)]
 struct CliArgs {
@@ -6,7 +7,24 @@ struct CliArgs {
 	pkgname: String
 }
 
+fn pkgmgr_found(p: &str) -> bool {
+	if Path::new(p).is_file() {
+		return true;
+	}
+	return false;
+}
+
 fn main() {
 	let args = CliArgs::parse();
 	println!("Command: {:?} Pkg: {:?}", args.rockcmd, args.pkgname);
+
+	if pkgmgr_found("/usr/bin/pacman") {
+		println!("Pacman found.");
+	}
+
+	match args.rockcmd.as_str() {
+		"i" => println!("-S"),
+		"install" => println!("-S"),
+		&_ => println!("Invalid"),
+	};
 }
